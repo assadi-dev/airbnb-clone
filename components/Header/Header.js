24f696a82f39052b43eff489f5694b25121eaa19
Assadi <assadi.halifa@gmail.com>
2021-11-05 15:07:44 +0100

    Update Header.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -83,7 +83,7 @@ const Header = ({ placeholder }) => {
       </div>
       {/** right section */}
       <div className="flex space-x-4 items-center justify-end">
-        <p className="hidden md:inline cursor-pointer ">Devenez hôte</p>
+        <p className="hidden md:inline cursor-pointer ">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
           <MenuIcon className="h-6" />
