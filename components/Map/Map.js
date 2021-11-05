47d0e63274f8a18ced1ab902038e1d96c41543b4
Assadi <assadi.halifa@gmail.com>
2021-11-05 14:43:52 +0100

    Update Map.js

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { getCenter } from "geolib";
+import Image from "next/image";
 
 function Map({ searchResults }) {
   //Get longitude and latitude object
@@ -20,7 +21,7 @@ function Map({ searchResults }) {
     zoom: 12,
   });
 
-  const [selectedState, setSelectedState] = useState();
+  const [selectedLocation, setSelectedLocation] = useState({});
 
   return (
     <ReactMapGL
@@ -37,8 +38,38 @@ function Map({ searchResults }) {
             offsetLeft={-20}
             offsetTop={-10}
           >
-            <p className="cursor-pointer text-2xl animate-bounce">🏠</p>
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              area-label="push-pin"
+            >
+              🏠
+            </p>
           </Marker>
+          {/** the popup that should show if we click on a marker */}
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+              className="z-50"
+            >
+              <div className=" flex flex-col justify-center relative px-2 ">
+                <Image
+                  src={result.img}
+                  objectFit="contain"
+                  layout="responsive"
+                  width={100}
+                  height={100}
+                />
+                <h3 className="font-semibold text-sm">{result.title}</h3>
+              </div>
+            </Popup>
+          ) : (
+            false
+          )}
         </div>
       ))}
     </ReactMapGL>
